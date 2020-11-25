450d494580779a61cf894ee7449695f3076980db
Saikat <saikatmandal8@gmail.com>
2020-11-25 07:41:40 +0600

    re-done MultipleDeletion

--- MultipleDeletion.cpp
+++ MultipleDeletion.cpp
@@ -2,28 +2,60 @@
 using namespace std;
 class Suvrodev
 {
+private:
+    int m_indexOffset = 0;
+
+    void swap(int& a, int& b);
+    void re_del(int data[], int& index, int size);
 public:
-    int i,Position;
-    void input();
-    void Deletion(int[],int,int);
+    void Delete_single(int data[], int& size, int index);
+    void Delete_multiple(int data[], int& data_size, int& amount);
+    void Delete_multiple2(int data[], int& data_size, int& amount);
+    void Output(int data[], int& index, int size);
 };
-void Suvrodev::Deletion(int Array[],int Limit,int Number)
-{
-    int count=0;
-    for(int j=1;j<=Number;j++)
-    {
-         cout<<j<<". Write index Number to delete=";
-         cin>>Position;
-         for(i=Position-count;i<=Limit;i++)
-         {
-           Array[i]=Array[i+1];
-         }
-       count++;
+void Suvrodev::swap(int& a, int& b){
+    int c = a;
+    a = b;
+    b = c;
+}
+void Suvrodev::re_del(int data[], int& index, int size){
+    if(index < size - 1){
+        swap(data[index], data[index + 1]);
+        re_del(data, ++index, size);
     }
-    cout<<"After Deletion....."<<endl;
-    for(i=1;i<=Limit-Number;i++)
-    {
-        cout<<i<<"."<<Array[i]<<" "<<endl;
+}
+void Suvrodev::Delete_single(int data[],int& size,int index)
+{
+    if(size > 1)
+        re_del(data, index, size);
+    size--;
+}
+//INDEX changes after every delete
+void Suvrodev::Delete_multiple(int data[], int& data_size, int& amount) {
+    if(amount){ //NEEDS NEW INDEX AFTER EVERY DELETE
+        int index;
+        cout << "Write the index to delete : ";
+        cin >> index;
+        Delete_single(data, data_size, index);
+        Delete_multiple(data, data_size, --amount);
+    }
+}
+void Suvrodev::Delete_multiple2(int data[], int& data_size, int& amount) {
+    if(amount){ //NEEDS ORIGINAL INDICES BEFORE CALLING THIS METHOD
+        int index;
+        cout << "Write the index to delete : ";
+        cin >> index;
+        Delete_single(data, data_size, index - m_indexOffset);
+        m_indexOffset++;
+        Delete_multiple2(data, data_size, --amount);
+    }
+    else
+        m_indexOffset = 0;
+}
+void Suvrodev::Output(int data[], int& index, int size) {
+    if(index < size) {
+        cout << data[index] << " ";
+        Output(data, ++index, size);
     }
 }
 int main()
@@ -35,15 +67,16 @@ int main()
     cin>>Limit;
     int Data[Limit];
     cout<<"Input Here\n";
-    for(int i=1;i<=Limit;i++)
+    for(int i=0;i<Limit;i++)
     {
         cout<<i<<". Enter Element=";
         cin>>Data[i];
     }
-    cout<<"How Many Element do you want to delete=";
+    cout<<"Number of Element do you want to delete=";
     cin>>Number;
-    s.Deletion(Data,Limit,Number);
-
+    s.Delete_multiple2(Data, Limit, Number);
+    int start = 0;
+    s.Output(Data, start, Limit);
     return 0;
 
 }
