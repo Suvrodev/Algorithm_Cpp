fa7f261fd4a8926002165f8166b0b26fed9e8cfd
Suvrodev Howlader <61430612+Suvrodev@users.noreply.github.com>
2021-01-17 23:11:00 +0600

    Add files via upload

--- MaxMin.cpp
+++ MaxMin.cpp
@@ -1,53 +1,64 @@
 #include<iostream>
 using namespace std;
-int Max,Min,Mid;
-void MaxMin(int Data[],int begin,int end)
+class Suvrodev
 {
-    if(begin==end){
-        Max=Min=Data[begin];
+public:
+    int Mid;
+    void MaxMin(int*,int,int,int&,int&);
+};
+void Suvrodev::MaxMin(int* Data,int begin,int end,int& max,int& min)
+{
+    if(begin==end)
+    {
+        max=min=Data[begin];
     }
     else if(begin==end-1)
     {
-        if(Data[begin]<Data[end])
+        if(Data[begin]>Data[end])
         {
-            Max=Data[end];
-            Min=Data[begin];
+            max=Data[begin];
+            min=Data[end];
         }
         else
         {
-            Max=Data[begin];
-            Min=Data[end];
+            max=Data[end];
+            min=Data[begin];
         }
     }
     else
     {
         Mid=(begin+end)/2;
-        MaxMin(Data,begin,Mid);
-        int MaxValue=Max;
-        int MinValue=Min;
-        MaxMin(Data,Mid+1,end);
-        if(MaxValue>Max){
-            Max=MaxValue;
-        }
-        if(MinValue<Min)
+        MaxMin(Data,begin,Mid,max,min);
+        int HelperMax;
+        int HelperMin;
+        MaxMin(Data,Mid+1,end,HelperMax, HelperMin);
+        if(HelperMax>max)
         {
-            Min=MinValue;
+            max=HelperMax;
+        }
+        if(HelperMin<min)
+        {
+            min=HelperMin;
         }
     }
 }
 int main()
 {
-    int begin=1,Limit;
-    cout<<"Enter Limit of Array:";
+    cout<<"MaxMin Algorithm"<<endl;
+    Suvrodev v;
+    int Limit,begin=1,max,min;
+    cout<<"Enter the Limit of Array:";
     cin>>Limit;
     int Data[Limit];
-    cout<<"Insert Data from Here"<<endl;
-    for(int i=1;i<=Limit;i++){
-        cout<<i<<". Input Data:";
+    cout<<"Enter Element from here"<<endl;
+    for(int i=begin;i<=Limit;i++)
+    {
+        cout<<i<<". Enter Input:";
         cin>>Data[i];
     }
-    MaxMin(Data,begin,Limit);
-    cout<<"Maximum Value: "<<Max<<endl;
-    cout<<"Minimum Value: "<<Min<<endl;
+    v.MaxMin(Data,begin,Limit,max,min);
+    cout<<"Maximum Value:"<<max<<endl;
+    cout<<"Minimum Value:"<<min<<endl;
+
     return 0;
 }
